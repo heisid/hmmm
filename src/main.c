abc99b62dd49052eb0e819ca61be277b2a4d3f14
Rosyid Haryadi <rosyid_haryadi@protonmail.com>
2023-12-23 13:26:57 +0700

    solid line

--- src/main.c
+++ src/main.c
@@ -57,10 +57,24 @@ void doDrawing(InitConfig config) {
 
     int end = config.SCREEN_WIDTH / 2;
     int start = -1 * end;
+    float prevX;
+    float prevY0, prevY1, prevY2;
     for (int x = start; x < end; x++) {
-        float y = 100 * sinf((float)x/10);
-        y *= -1;
-        DrawPixelV(Vector2Add((Vector2){(float)x, y}, (Vector2){(float)config.ORIGIN_X, (float)config.ORIGIN_Y}), GREEN);
+        float y0 = 100 * sinf((float)x / 10);
+        float y1 = 100 * cosf((float)x / 20);
+        float y2 = y0 + y1;
+        y0 *= -1;
+        y1 *= -1;
+        y2 *= -1;
+        if (x != start) {
+//            DrawLineV((Vector2){prevX, prevY0}, Vector2Add((Vector2){(float)x, y0}, (Vector2){(float)config.ORIGIN_X, (float)config.ORIGIN_Y}), GREEN);
+//            DrawLineV((Vector2){prevX, prevY1}, Vector2Add((Vector2){(float)x, y1}, (Vector2){(float)config.ORIGIN_X, (float)config.ORIGIN_Y}), BLUE);
+            DrawLineV((Vector2){prevX, prevY2}, Vector2Add((Vector2){(float)x, y2}, (Vector2){(float)config.ORIGIN_X, (float)config.ORIGIN_Y}), ORANGE);
+        }
+        prevX = (float)config.ORIGIN_X + (float)x;
+        prevY0 = (float)config.ORIGIN_Y + y0;
+        prevY1 = (float)config.ORIGIN_Y + y1;
+        prevY2 = (float)config.ORIGIN_Y + y2;
     }
 
     EndDrawing();
